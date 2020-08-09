1f216ce007ecd8a087da19616cc75a3e250f6f71
raphaelletseng <raphaelle.tseng@mail.mcgill.ca>
2020-08-09 17:12:03 -0400

    FAQ Accordion added

--- src/nav/Navbar.jsx
+++ src/nav/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { css } from '@emotion/core';
+import { jsx, css } from '@emotion/core';
 import styled from '@emotion/styled';
 //import detectPassiveEvents from 'detect-passive-events';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
